f05859788e69d17eff859b80716995596567830e
David Francos Cuartero <work@davidfrancos.net>
2013-09-23 14:16:55 +0200

    Added GPL3 , only copyright left to add

--- arduino_mindwave/mindwave_test.ino
+++ arduino_mindwave/mindwave_test.ino
@@ -1,3 +1,19 @@
+/*
+    Copyright
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #define BAUDRATE 115200
 #define powercontrol 10
 
@@ -61,9 +77,9 @@ byte ReadOneByte() {
 
 
 void flash() {
-  digitalWrite(led, HIGH); 
-    delay(5);              
-  digitalWrite(led, LOW);  
+  digitalWrite(led, HIGH);
+    delay(5);
+  digitalWrite(led, LOW);
 }
 
 void loop() {
